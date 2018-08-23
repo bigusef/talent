adf59070dfeea1bf0cd2a9ffb3306a4560fac157
Mahmud Youssef <mahmud.youssef@icloud.com>
2018-08-23 14:17:14 +0200

    change and delete university and department model

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Expert, Tutorial
+
+
+@admin.register(Expert)
+class ExpertAdmin(admin.ModelAdmin):
+    list_display = 'name', 'university', 'department', 'rate', 'updated'
+
+
+@admin.register(Tutorial)
+class TutorialAdmin(admin.ModelAdmin):
+    list_display = 'name', 'expert', 'hours', 'updated'
--- backend/models.py
+++ backend/models.py
@@ -1,50 +1,41 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from datetime import datetime
+# from PIL import Image
 
 
-class University(models.Model):
-    name = models.CharField(max_length=50)
-    timestamps = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.name
-
-
-class Department(models.Model):
-    name = models.CharField(max_length=100)
-    university = models.ForeignKey(University, on_delete=models.CASCADE)
-    timestamps = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.name
+def upload_dir(instance, filename):
+    _today = datetime.now().strftime('%m%d%Y')
+    return f'profile_img/{instance.pk}_{_today}_{filename}'
 
 
 class Expert(models.Model):
-    def upload_dir(instance, filename):
-        return f'profile_img/i-{instance.name}-{filename}'
-
-    name = models.CharField(max_length=150)
-    university = models.ForeignKey(University, on_delete=models.CASCADE)
-    department = models.ForeignKey(Department, on_delete=models.CASCADE)
-    img = models.ImageField(width_field=100, height_field=100, upload_to=upload_dir)
+    name = models.CharField(max_length=50)
+    university = models.CharField(max_length=50)
+    department = models.CharField(max_length=100)
+    img = models.ImageField(upload_to=upload_dir)
     rate = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(5.0)])
-    hours = models.IntegerField()
     timestamps = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=True, verbose_name='Last Update')
 
     def __str__(self):
         return self.name
+
+    # def save(self, *args, **kwargs):
+    #     super(Expert, self).save(*args, **kwargs)
+    #     image = Image.open(self.img.path)
+    #     image = image.resize((200, 200))
+    #     image.save(self.img.path)
 
 
 class Tutorial(models.Model):
     name = models.CharField(max_length=150)
     expert = models.OneToOneField(Expert, on_delete=models.CASCADE)
+    hours = models.IntegerField()
     price = models.IntegerField()
     description = models.TextField()
     timestamps = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=True, verbose_name='Last Update')
 
     def __str__(self):
         return self.name
