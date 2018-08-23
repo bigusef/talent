38494ab83e31890a39f60c75658fe07277d86f45
Mahmud Youssef <mahmud.youssef@icloud.com>
2018-08-23 12:32:45 +0200

    add media file setting

--- talent/settings/base.py
+++ talent/settings/base.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third-party app
+    'rest_framework',
+
+    # project app
+    'backend',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
